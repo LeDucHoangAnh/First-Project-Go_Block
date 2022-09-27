55f2475e65c62d6b9065658cb1aa3cd1994f7ce4
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-28 05:31:32 +0700

    Addblock, Genesisblock và Initblock

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"crypto/sha256"
 )
 
+type BlockChain struct{
+	blocks []*Block
+}
+
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -23,6 +27,25 @@ func CreateBlock(data string, prevHash []byte *Block){
 	return block
 }
 
+func (chain *BlockChain) AddBlock(data string)  {
+	prevBlock := chain.blocks[len(chain.blocks) -1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block{
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain  {
+	return &BlockChain{[] * Block{Genesis()}}
+}
+
 func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
 
 }
