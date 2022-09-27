81438e2be9959dcc427e6674cc5250b3485cc7f4
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-28 05:36:20 +0700

    Chỉnh sửa

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
-type BlockChain struct{
+type BlockChain struct {
 	blocks []*Block
 }
 
@@ -16,29 +17,29 @@ type Block struct {
 }
 
 func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte[b.Data, b.PrevHash], []byte{})
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }
 
-func CreateBlock(data string, prevHash []byte *Block){
-	block := &Block{[]block{}, []byte(data), prevHash}
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
 	block.DeriveHash()
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string)  {
-	prevBlock := chain.blocks[len(chain.blocks) -1]
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, new)
 }
 
-func Genesis() *Block{
+func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain  {
-	return &BlockChain{[] * Block{Genesis()}}
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
 }
 
 func main() {
@@ -47,5 +48,11 @@ func main() {
 	chain.AddBlock("First Block")
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 
 }
